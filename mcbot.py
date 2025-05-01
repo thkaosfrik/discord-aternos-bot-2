5e4a2856055c65bd6fdaa3419e068e514aa03600
LukiUWUX3 <lukasmosler137@gmail.com>
2025-05-01 15:23:19 +0200

    fix 2

--- mcbot.py
+++ mcbot.py
@@ -7,6 +7,7 @@ import asyncio
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.default()
+intents.message_content = True
 bot = commands.Bot(command_prefix='?', intents=intents)
 
 @bot.command()
